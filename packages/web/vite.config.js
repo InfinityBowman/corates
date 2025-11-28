272bb737822b4720704b02c5e0ed0d59c7a220eb
Jacob Maynard <jacobamaynard@proton.me>
2025-11-28 10:25:52 -0500

    remove smtp server, add some testing setup

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -33,5 +33,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    setupFiles: ['./src/__tests__/setup.js'],
+    include: ['src/**/*.{test,spec}.{js,jsx}'],
   },
 });
