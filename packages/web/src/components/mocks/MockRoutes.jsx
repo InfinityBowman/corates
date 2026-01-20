8aec325d130da36f84e3c4f99374958fab1bfea8
Jacob Maynard <jacobamaynard@proton.me>
2026-01-19 21:27:31 -0600

    remove more mocks

--- packages/web/src/components/mocks/MockRoutes.jsx
+++ packages/web/src/components/mocks/MockRoutes.jsx
@@ -5,13 +5,9 @@ import { Route } from '@solidjs/router';
 import MockIndex from './MockIndex.jsx';
 
 // Lazy-load all mock page components
-const ProjectViewEditorial = lazy(() => import('./ProjectViewEditorial.jsx'));
-const ProjectViewDashboard = lazy(() => import('./ProjectViewDashboard.jsx'));
-const ProjectViewKanban = lazy(() => import('./ProjectViewKanban.jsx'));
 const ProjectViewComplete = lazy(() => import('./ProjectViewComplete.jsx'));
 const DashboardMock = lazy(() => import('./DashboardMock.jsx'));
 const ReactivePropsDemo = lazy(() => import('./ReactivePropsDemo.jsx'));
-const ProjectWizardMock = lazy(() => import('./ProjectWizardMock.jsx'));
 const ProjectViewV2 = lazy(() => import('./ProjectViewV2.jsx'));
 
 /**
@@ -22,13 +18,9 @@ export default function MockRoutes() {
   return (
     <Route path='/mocks'>
       <Route path='/' component={MockIndex} />
-      <Route path='/project-view-editorial' component={ProjectViewEditorial} />
-      <Route path='/project-view-dashboard' component={ProjectViewDashboard} />
-      <Route path='/project-view-kanban' component={ProjectViewKanban} />
       <Route path='/project-view-complete' component={ProjectViewComplete} />
       <Route path='/dashboard' component={DashboardMock} />
       <Route path='/reactivity-example' component={ReactivePropsDemo} />
-      <Route path='/project-wizard' component={ProjectWizardMock} />
       <Route path='/project-view-v2' component={ProjectViewV2} />
     </Route>
   );
